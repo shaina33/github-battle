a3af8beb8a242838cf41296f6f7fbb0a95e3cab2
Shaina Karasin <skarasin39@gmail.com>
2018-02-19 15:03:47 -0500

    added ReactRouter & 3 main Routes

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,29 @@
 var React = require('react');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+var Nav = require('./Nav');
+var Home = require('./Home');
 var Popular = require('./Popular');
+var Battle = require('./Battle');
 
 class App extends React.Component {
     render() {
         return (
-            <div className='container'>
-                <Popular />
-            </div>
+          <Router>
+              <div className='container'>
+                  <Nav />
+                  <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/popular' component={Popular} />
+                    <Route path='/battle' exact component={Battle} />
+                    <Route render={ function() {
+                      return <p>Not Found</p>
+                    }} />
+                  </Switch>
+              </div>
+          </Router>
         )
     }
 }
